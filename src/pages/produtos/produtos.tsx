53604fa1ce3e8f535adf3d12595467d36463dd23
GisellyRock <gisellydsr@gmail.com>
2023-11-19 11:14:40 -0300

    feat:userRouter

--- src/pages/produtos/produtos.tsx
+++ src/pages/produtos/produtos.tsx
@@ -1,32 +1,38 @@
 import Card from "@/components/Card";
 import Topo from "@/components/Topo";
+import { useRouter } from "next/router";
 const produtos = [
   // Seu array de produtos aqui
   {
+    id:1,
     produto: 'Mouse',
     valor: 49.9,
     desconto: 0,
     disponível: true,
   },
   {
+    id:2,
     produto: 'Teclado',
     valor: 69.9,
     desconto: 0,
     disponível: true,
   },
   {
+    id:3,
     produto: 'Monitor',
     valor: 449.9,
     desconto: 0,
     disponível: false,
   },
   {
+    id:4,
     produto: 'CPU',
     valor: 4992.9,
     desconto: 0,
     disponível: true,
   },
   {
+    id:5,
     produto: 'Caixa de som',
     valor: 49.9,
     desconto: 0,
@@ -43,6 +49,13 @@ function caldDesc2(v: number, d: number) {
 }
 
 export default function produtosPagina(){
+  const router= useRouter()
+  // const nome=router.query.nome
+  // const curso=router.query.curso
+  const {nome, curso}=router.query
+  console.log(nome)
+  console.log(curso)
+
   return(
     <div>
     <Topo/>
@@ -50,7 +63,7 @@ export default function produtosPagina(){
     {produtos.map((e: any) =>
       e.disponível ? (
         <Card
-          key={e.produto}
+          key={e.id}
           produto={e.produto}
           valor={e.valor}
           desconto={e.desconto}
