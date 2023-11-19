3276ec807b7d1a056550298c7f96d2646df7252e
GisellyRock <gisellydsr@gmail.com>
2023-11-19 10:03:13 -0300

    feat:array de produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,82 @@
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-// const inter = Inter({ subsets: ['latin'] })
+// Importe as dependências necessárias aqui, se necessário
 
-import Topo from "@/components/Topo"
-import Card from "@/src/components/Card"
+import Topo from "@/components/Topo";
+import Card from "@/src/components/Card";
 
-const nome='Giselly'
-let canal='CBFCursos'
+const nome = 'Giselly';
+let canal = 'CBFCursos';
 
-function caldDesc(v:number, d:number){
-  return v-d 
+const produtos = [
+  // Seu array de produtos aqui
+  {
+    produto:'Mouse',
+    valor:49.9,
+    desconto:0,
+    disponível:true
+  },
+  {
+    produto:'Teclado',
+    valor:69.9,
+    desconto:0,
+    disponível:true
+  },
+  {
+    produto:'Monitor',
+    valor:449.9,
+    desconto:0,
+    disponível:true
+  },
+  {
+    produto:'CPU',
+    valor:4992.9,
+    desconto:0,
+    disponível:true
+  },
+  {
+    produto:'Caixa de som',
+    valor:49.9,
+    desconto:0,
+    disponível:true
+  }
+];
+
+function caldDesc(v: number, d: number) {
+  return v - d;
 }
 
-function caldDesc2(v:number, d:number){
-  return v-d/2 
+function caldDesc2(v: number, d: number) {
+  return v - d / 2;
 }
-
 
 export default function Home() {
   return (
     <div>
-      <Topo/>
+      <Topo />
       <div style={testecss}>
-      <div>Cursos de react Next</div>
-      <div>Typescript</div>
-      <div style={{color:'#f00', backgroundColor:'#bbb'}}>React
-      </div>
+        <div>Cursos de react Next</div>
+        <div>Typescript</div>
+        <div style={{ color: '#f00', backgroundColor: '#bbb' }}>React</div>
       </div>
       <div className="flex justify-center gap-3">
-        <Card produto={'Mouse'} valor={49.90} desconto={10} funcao={caldDesc}/>
-        <Card produto={'Teclado'} valor={49.90} desconto={2} funcao={caldDesc}/>
-        <Card produto={'Monitor'} valor={49.90} desconto={0} funcao={caldDesc2}/>
-        <Card produto={'CPU'} valor={49.90} desconto={15} funcao={caldDesc2}/>
-
+        {produtos.map((e: any) => (
+          <Card
+            key={e.produto} // Adicione uma chave única (neste caso, o nome do produto) para cada item do array
+            produto={e.produto}
+            valor={e.valor}
+            desconto={e.desconto}
+            funcao={caldDesc} // Corrigido para usar a função caldDesc
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-const testecss={
-  display:'flex',
-  justifyContent:'center',
-  alignItems:'center',
-  color:'$00f',
-  backgroundColor:'#eee',
-  fontSize:'20px'
-}
+const testecss = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: '#00f', // Corrigido de '$00f' para '#00f'
+  backgroundColor: '#eee',
+  fontSize: '20px',
+};
--- src/src/components/Card.tsx
+++ src/src/components/Card.tsx
@@ -1,23 +1,21 @@
-interface CardProps{
-    produto:string,
-    valor:number,
-    desconto:number,
-    funcao:any 
+interface CardProps {
+  produto: string;
+  valor: number;
+  desconto: number;
+  funcao: any;
 }
 
-export default function Card(props:CardProps){
-  return(
-    <div className="flex flex-col border border-red-700 rounded-sm p-1">
-      <div>Produto:{props.produto}</div>
-      <div>Valor:R${props.valor}</div>
-      {props.desconto>0?(
+export default function Card(props: CardProps) {
+  return (
+    <div className={`flex flex-col border-4 ${props.desconto > 0 ? 'border-red-700' : 'border-blue-700'} rounded-sm p-1`}>
+      <div>Produto: {props.produto}</div>
+      <div>Valor: R${props.valor}</div>
+      {props.desconto > 0 && (
         <div>
-        <div>Desconto:R${props.desconto}</div>
-        <div>Preço Venda:R${props.funcao(props.valor, props.desconto)}</div>
+          <div>Desconto: R${props.desconto}</div>
+          <div>Preço Venda: R${props.funcao(props.valor, props.desconto)}</div>
         </div>
-      ):<div> sem desconto </div>}(
-      
-      )
-      </div>
-  )
+      )}
+    </div>
+  );
 }
