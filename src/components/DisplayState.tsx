97bc3635fd95a071f4b7b668dacab55b3a1569f8
GisellyRock <gisellydsr@gmail.com>
2023-11-19 13:50:17 -0300

    feat:funcoes para componentes

--- src/components/DisplayState.tsx
+++ src/components/DisplayState.tsx
@@ -1,20 +1,32 @@
 interface DisplayStateProps {
   valor: number;
-  fadicionar: any;
-  fsubtrair: any;
+  fvalor:any
 }
 
 export default function DisplayState(props: DisplayStateProps) {
+
+  function adicionar(){
+    let c=props.valor
+    c++
+    props.fvalor(c)
+  }
+
+  function subtrair(){
+    let c=props.valor
+    c--
+    props.fvalor(c)
+  }
+
   return (
     <div className="flex flex-col p-5 rounded-md bg-zinc-300 justify-center items-center w-[300px]">
       <div className="flex bg-zinc-700 text-green-500 justify-center items-center w-[200px] mb-3">
         {props.valor}
         </div>
         <div className="flex gap-5">
-          <button className="btnPadrao" onClick={props.fsubtrair}>
+          <button className="btnPadrao" onClick={subtrair}>
             sub
           </button>
-          <button className="btnPadrao" onClick={props.fadicionar}>
+          <button className="btnPadrao" onClick={adicionar}>
             add
           </button>
         </div>
--- src/pages/usestate/usestate.tsx
+++ src/pages/usestate/usestate.tsx
@@ -4,21 +4,10 @@ import DisplayState from "@/components/DisplayState"
 export default function usestate(){
   const [cont, setCont]=useState<number>(10)
 
-  function adicionar(){
-    let c=cont
-    c++
-    setCont(c)
-  }
-
-  function subtrair(){
-    let c=cont
-    c--
-    setCont(c)
-  }
   return(
     <div>
       usestate
-      <DisplayState valor={cont} fadicionar={adicionar} fsubtrair={subtrair}></DisplayState>
+      <DisplayState valor={cont} fvalor={setCont}></DisplayState>
     </div>
   )
 }
